80ee615b2ab41c82a060de3ae2faa15ddef0cbe8
Eddie Hudson <33990581+codedmty@users.noreply.github.com>
2018-01-01 09:33:32 -0600

    Add files via upload
    Added alert message if required fields are left blank.

--- main.js
+++ main.js
@@ -15,18 +15,33 @@ if (!registerEarly) {
   raceNumber += 1000;
 }
 
-if (runnerName != '') {
+if (runnerName != '' && runnerAge != '') {
 if (runnerAge >= 18 && registerEarly) {
+  document.getElementById('alert').innerHTML = '';
   document.getElementById('output').innerHTML = `${runnerName}, your number is ${raceNumber}.<br> Your race time is 9:30am`;
 } else if (runnerAge >= 18 || registerEarly) {
+  document.getElementById('alert').innerHTML = '';
   document.getElementById('output').innerHTML = `${runnerName}, your number is ${raceNumber}.<br> Your race time is 11:00am`;
 } else if (runnerAge < 18 && !registerEarly) {
+  document.getElementById('alert').innerHTML = '';
   document.getElementById('output').innerHTML = `${runnerName}, your number is ${raceNumber}.<br> Your race time is 12:30pm`;
 } else {
+  document.getElementById('alert').innerHTML = '';
   document.getElementById('output').innerHTML = `Please see the registration desk.`;
 }
+} else if (runnerName === '' && runnerAge === '') {
+    document.getElementById('alert').innerHTML = `Enter your name, and age.`;
+    document.getElementById('output').innerHTML = '';
+} else if  (runnerName === '' && runnerAge != '') {
+    document.getElementById('alert').innerHTML = `Enter your name.`;
+    document.getElementById('output').innerHTML = '';
+} else if  (runnerName != '' && runnerAge === '') {
+    document.getElementById('alert').innerHTML = `Enter your age.`;
+    document.getElementById('output').innerHTML = '';
 } else {
-    document.getElementById('output').innerHTML = `Please enter your name.`;
+    document.getElementById('alert').innerHTML = `Please see the registration desk.`;
+    document.getElementById('output').innerHTML = '';
 }
+    
 return false;
 }
